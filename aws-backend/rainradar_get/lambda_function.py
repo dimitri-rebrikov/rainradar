b49278de4befab274d427c6f50d70445a1bbf274
Dimitri Rebrikov <dimitri.rebrikov@gmx.de>
2022-07-04 20:01:40 +0200

    switched from aws' simpledb to s3

--- aws-backend/rainradar_get/lambda_function.py
+++ aws-backend/rainradar_get/lambda_function.py
@@ -1,21 +1,45 @@
+import csv
 import json
 import boto3
+import io
 
-db = boto3.resource("dynamodb")
+client = boto3.client('s3')
+
 
 def lambda_handler(event, context):
-    assert 'queryStringParameters' in event, "missing plz parameter"
-    queryParams = event['queryStringParameters']
-    assert 'plz' in queryParams, "missing plz parameter"
-    plz = int(queryParams['plz'])
-
-    rvTable = db.Table("rainradar_rv")
-    try:
-        return rvTable.get_item(Key = {'plz':plz})['Item']["info"]
-    except db.Client.exceptions.ResourceNotFoundException:
+    if  'queryStringParameters' not in event:
         return {
             "statusCode": 400,
             "body": json.dumps({
-                "error": "plz " + str(plz) + " not found"
+                "error": "missing plz parameter"
             })
         }
+        
+    queryParams = event['queryStringParameters']
+
+    if 'plz' not in queryParams:
+        return {
+            "statusCode": 404,
+            "body": json.dumps({
+                "error": "missing plz parameter"
+            })
+        }
+
+    plz = int(queryParams['plz'])
+
+    for row in csv.reader(
+        io.StringIO(
+            client.get_object(
+                Bucket='rainradar',
+                Key='rainradar_rv_data'
+            )['Body'].read().decode()
+        )
+    ):
+        if int(row[0]) == plz:
+            return row[1:]
+    return {
+        "statusCode": 404,
+        "body": json.dumps({
+            "error": "plz " + str(plz) + " not found"
+        })
+    }
