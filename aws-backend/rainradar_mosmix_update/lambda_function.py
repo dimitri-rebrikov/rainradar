e4f3c73640fbfe0c113f622af3708662eb86e0de
Dimitri Rebrikov <dimitri.rebrikov@gmx.de>
2022-11-06 20:37:43 +0100

    consider the first exectution of the lamda function

--- aws-backend/rainradar_mosmix_update/lambda_function.py
+++ aws-backend/rainradar_mosmix_update/lambda_function.py
@@ -8,14 +8,17 @@ def lambda_handler(event, context):
 
     print("Timestamp of the latest mosmix data:" + str(mosmixTimestamp))
 
-    lastTimestamp=client.get_object(
-        Bucket='rainradar',
-        Key='rainradar_mosmix_data'
-    )['LastModified'].replace(tzinfo=None)
+    lastTimestamp=None
+    try:
+        lastTimestamp=client.get_object(
+            Bucket='rainradar',
+            Key='rainradar_mosmix_data'
+        )['LastModified'].replace(tzinfo=None)
+        print("Timestamp of the last exectution:" + str(lastTimestamp))
+    except client.exceptions.NoSuchKey:
+        print("No last exectution")
 
-    print("Timestamp of the tast exectution:" + str(lastTimestamp))
-
-    if(mosmixTimestamp > lastTimestamp):
+    if lastTimestamp is None or mosmixTimestamp > lastTimestamp:
         with mosmix_data.mosmixDataStream() as file:
             client.put_object( 
                 Bucket='rainradar',
